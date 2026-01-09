dc734c2905c98f9deedb302bdc32622c4df140cb
KartikBhatnagar <kartikbhatnagar15@gmail.com>
2026-01-09 17:33:56 +0530

    subprocess module

--- subprocess_module/run_timer.py
+++ subprocess_module/run_timer.py
@@ -3,4 +3,5 @@ import subprocess
 print("Starting the timer program")
 subprocess.run(["python","timer.py","4"])
 print("Timer program completed")
-
+lis = ["python","timer.py","4"]
+subprocess.run(lis)
